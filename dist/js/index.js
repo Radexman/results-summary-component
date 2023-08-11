99f0273a96bbf7b68c0699600b623d64d1d50540
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-11 13:59:04 +0200

    MVP

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,27 @@
 "use strict";
-const msg = 'Typescript compilation is on.';
-console.log(msg);
+const fetchData = async () => {
+    const res = await fetch('./data.json');
+    const data = await res.json();
+    createStats(data);
+    console.log(data[0]);
+};
+const createStats = (data) => {
+    const statsContainer = document.getElementById('stats-container');
+    data.forEach((stat) => {
+        const div = document.createElement('div');
+        div.classList.add('single-stat');
+        div.innerHTML = `
+            		<div class="flex gap-2">
+						<img src="${stat.icon}" alt="" />
+						<h3>${stat.category}</h3>
+					</div>
+					<div class="font-semibold">
+						<span>${stat.score}</span>
+						<span class="text-neutral-darkGrayBlue"> / 100</span>
+					</div>
+        `;
+        statsContainer?.appendChild(div);
+    });
+};
+fetchData();
 //# sourceMappingURL=index.js.map
