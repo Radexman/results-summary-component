8f2fba907b4a491feb461adc038416b78e93b793
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-11 15:07:08 +0200

    Conditional Styling

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,27 +1,64 @@
 "use strict";
+// Fetching JSON Data
 const fetchData = async () => {
     const res = await fetch('./data.json');
     const data = await res.json();
     createStats(data);
-    console.log(data[0]);
 };
+// Render output
 const createStats = (data) => {
     const statsContainer = document.getElementById('stats-container');
     data.forEach((stat) => {
-        const div = document.createElement('div');
-        div.classList.add('single-stat');
-        div.innerHTML = `
-            		<div class="flex gap-2">
-						<img src="${stat.icon}" alt="" />
-						<h3>${stat.category}</h3>
-					</div>
-					<div class="font-semibold">
-						<span>${stat.score}</span>
-						<span class="text-neutral-darkGrayBlue"> / 100</span>
-					</div>
-        `;
-        statsContainer?.appendChild(div);
+        // Wrapper Div
+        const wrapperDiv = document.createElement('div');
+        wrapperDiv.classList.add('single-stat');
+        // Flex Item One
+        const innerDivOne = document.createElement('div');
+        innerDivOne.classList.add('inner-div-one');
+        wrapperDiv.appendChild(innerDivOne);
+        // Flex Item One Content
+        const statSvg = document.createElement('img');
+        statSvg.setAttribute('src', stat.icon);
+        innerDivOne.appendChild(statSvg);
+        const statName = document.createElement('h3');
+        statName.textContent = stat.category;
+        // Conditional Styling
+        conditionalStyling(stat.category, statName);
+        innerDivOne.appendChild(statName);
+        // Flex Item Two
+        const innerDivTwo = document.createElement('div');
+        innerDivTwo.classList.add('font-semibold');
+        wrapperDiv.appendChild(innerDivTwo);
+        // Flex Item Two Content
+        const statScore = document.createElement('span');
+        statScore.textContent = stat.score;
+        innerDivTwo.appendChild(statScore);
+        const statMax = document.createElement('span');
+        statMax.classList.add('text-neutral-darkGrayBlue');
+        statMax.textContent = ' / 100';
+        innerDivTwo.appendChild(statMax);
+        statsContainer?.appendChild(wrapperDiv);
     });
+};
+const conditionalStyling = (itemCategory, DOMElement) => {
+    let color;
+    switch (itemCategory) {
+        case 'Reaction':
+            color = 'hsl(0, 100%, 67%)';
+            break;
+        case 'Memory':
+            color = 'hsl(39, 100%, 56%)';
+            break;
+        case 'Verbal':
+            color = 'hsl(166, 100%, 37%)';
+            break;
+        case 'Visual':
+            color = 'hsl(234, 85%, 45%)';
+            break;
+        default:
+            color = 'text-slate-800';
+    }
+    DOMElement.style.color = color;
 };
 fetchData();
 //# sourceMappingURL=index.js.map
